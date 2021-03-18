240a3d8ed1fe9a1fe6b0e776eba662f5f4054820
FelixLee-hub12 <felixlkw1219@gmail.com>
2021-03-18 21:55:18 +0800

    test

--- frontend/src/components/verticalMoreButton.js
+++ frontend/src/components/verticalMoreButton.js
@@ -3,7 +3,7 @@ import IconButton from '@material-ui/core/IconButton';
 import Menu from '@material-ui/core/Menu';
 import MenuItem from '@material-ui/core/MenuItem';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
-
+import AddAnnotationDialog from './createAnnotation';
 const options = [
     'Add annotation',
     'View annotation',
@@ -13,12 +13,28 @@ const options = [
 const VerticalMoreButton = () => {
     const [anchorEl, setAnchorEl] = useState(null);
     const open = Boolean(anchorEl);
+    const [dialogOpen, setDialogOpen] =useState(false);
+
+    const handleDialogOpen = () => {
+        setDialogOpen(true);
+    }
+    
+    const closeDialog = () => {
+        setDialogOpen(false);
+    }
 
     const handleClick = (e) => {
         setAnchorEl(e.currentTarget);
     }
 
     const handleClose = () => {
+        setAnchorEl(null);
+    }
+
+    const handleMenuItemClick = (e) => {
+        if (e.target.textContent === 'Add annotation'){
+            handleDialogOpen();
+        }
         setAnchorEl(null);
     }
 
@@ -41,14 +57,19 @@ const VerticalMoreButton = () => {
         onClose={handleClose}
         >
         {options.map((option) => (
-            <MenuItem key={option} onClick={handleClose}>
+            <MenuItem key={option} onClick={(e) => handleMenuItemClick(e)}>
                 {option}
             </MenuItem>
         ))}
         </Menu>
+        <AddAnnotationDialog
+        open={dialogOpen}
+        setOpen={setDialogOpen}
+        >
+            
+        </AddAnnotationDialog>
         </div>
-        
-        
+       
     );
 }
 
