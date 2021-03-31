514315b4628559cef5801ba84f4f1d0990dddb70
FelixLee-hub12 <felixlkw1219@gmail.com>
2021-04-01 01:42:38 +0800

    api correction

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -5,7 +5,8 @@ var postController = require('../controllers/posts.js')
 const router = express.Router();
 
 router.get('/list', postController.getPosts);
-router.post('/create', postController.createPost);
+
+router.put('/post', postController.createPost);
 router.delete('/delete/:id', postController.deletePost);
 
 module.exports = router;
--- frontend/src/context/api/index.js
+++ frontend/src/context/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-const url_posts = 'http://localhost:5000/post/list';
+const url_posts = 'http://localhost:5000/post';
 
-export const fetchPosts = () => axios.get(url_posts);
+export const fetchPosts = () => axios.get(`${url_posts}/list`);
 export const createPost = (newPost) => axios.put(url_posts, newPost);
 export const updatePost = (id,updatePost) => axios.patch(`${url_posts}/${id}`, updatePost);
