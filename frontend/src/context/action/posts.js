01ba17317c410111afcc5b27e63d3dc4f99594e4
FelixLee-hub12 <felixlkw1219@gmail.com>
2021-04-01 01:22:11 +0800

    schema change

--- frontend/src/context/action/posts.js
+++ frontend/src/context/action/posts.js
@@ -3,7 +3,7 @@ import * as api from '../api/index.js';
 export const getPosts = () => async (dispatch) => {
   try {
     const { data } = await api.fetchPosts();
-
+    console.log(data);
     dispatch({ type: 'FETCH_ALL', payload: data });
   } catch (error) {
     console.log(error.message);
@@ -13,7 +13,7 @@ export const getPosts = () => async (dispatch) => {
 export const createPost = (post) => async (dispatch) => {
   try {
     const {data} = await api.createPost(post);
-
+   
     dispatch({type: 'CREATE', payload: data});
   } catch (error){
     console.log(error.message);
