beb3a4b6b63d16c3f2cfb19423ed8551bb3cb7c8
Lsy322 <j3a2s2o2n8@gmail.com>
2021-04-20 11:52:32 +0800

    Minor bug fixes

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -8,6 +8,8 @@ const client = mongoose.connection;
 const userCollection = client.collection("user")
 const postCollection = client.collection("posts")
 
+const prefix = "auth0|"
+
 //#region token
 const getToken = () =>{
     var request = require("request");
@@ -34,8 +36,8 @@ const Mutifetch = (friendArray,TOKEN) =>{
     return new Promise(function (resolve,reject){
         for (let index = 0; index < friendArray.length; index++) {
             var element = friendArray[index];
-            if (element.indexOf("auth0|") == -1){
-                element = "auth0|" + element
+            if (element.indexOf(prefix) == -1){
+                element = prefix + element
             }
             var options = { method: 'GET',
             url: 'https://dev-1ksx3uq3.us.auth0.com/api/v2/users/'+ element,
@@ -67,11 +69,15 @@ getUser: async (req,res)=>{
         return;
     }
     var doc = await userCollection.findOne({_id:mongoose.Types.ObjectId(req.params.id)})
+    if (doc == null){
+        res.json({message:"No such user"})
+        return;
+    }
     getToken().then((data)=>{
         TOKEN = data.access_token
         if (doc != null){
             var options = { method: 'GET',
-            url: 'https://dev-1ksx3uq3.us.auth0.com/api/v2/users/'+ "auth0|" + req.params.id,
+            url: 'https://dev-1ksx3uq3.us.auth0.com/api/v2/users/'+ prefix + req.params.id,
             headers: { 'authorization':'Bearer ' + TOKEN, 'content-type': 'application/json' },
             body: {},
             json:true};
@@ -90,8 +96,20 @@ addFriendRequest: async (req,res)=>{
     try{
         const rkey = req.body.RecieveId
         const skey = req.body.SendId
-        userCollection.updateOne({_id:mongoose.Types.ObjectId(rkey)},{$push:{friendRequest:skey}})
-        res.json({message:"Request added"})
+        const friendCheck = await userCollection.findOne({_id:mongoose.Types.ObjectId(rkey),friends:prefix + skey})
+        if (friendCheck != null){
+            res.json({message:"Request can't be sent to a friend"})
+            return;
+        }
+
+        const requestCheck = await userCollection.findOne({_id:mongoose.Types.ObjectId(rkey),friendRequest:skey})
+        if (requestCheck == null){
+            userCollection.updateOne({_id:mongoose.Types.ObjectId(rkey)},{$push:{friendRequest:skey}})
+            res.json({message:"Request added"})
+        }else{
+            res.json({message:"Request already exist"})
+        }
+        
     }catch (err){
         res.json(err)
     }
@@ -100,8 +118,13 @@ removeFriendRequest: async(req,res)=>{
     try{
         const rkey = req.body.RecieveId
         const skey = req.body.RemoveId
-        userCollection.updateOne({_id:mongoose.Types.ObjectId(rkey)},{$pull:{friendRequest:skey}})
-        res.json({message:"Request removed"})
+        const check = await userCollection.findOne({_id:mongoose.Types.ObjectId(rkey),friendRequest:skey})
+        if (check != null){
+            userCollection.updateOne({_id:mongoose.Types.ObjectId(rkey)},{$pull:{friendRequest:skey}})
+            res.json({message:"Request removed"})
+        }else{
+            res.json({message:"No such request to remove"})
+        }
     }catch(err){
         res.json(err)
     }
@@ -110,9 +133,15 @@ acceptFriendRequest: async (req,res)=>{
     try{
         const uid = req.body.uid
         const aid = req.body.acceptedId
-        userCollection.updateOne({_id:mongoose.Types.ObjectId(uid)},{$push:{friends:'auth0|'+ aid},$pull:{friendRequest:aid}})
-        userCollection.updateOne({_id:mongoose.Types.ObjectId(aid)},{$push:{friends:'auth0|'+uid}})
-        res.json({message:"Friend request accepted"})
+        const check = await userCollection.findOne({_id:mongoose.Types.ObjectId(uid),friendRequest:aid})
+        if (check != null){
+            userCollection.updateOne({_id:mongoose.Types.ObjectId(uid)},{$push:{friends:prefix+ aid},$pull:{friendRequest:aid}})
+            userCollection.updateOne({_id:mongoose.Types.ObjectId(aid)},{$push:{friends:prefix+uid}})
+            res.json({message:"Friend request accepted"})
+        }else{
+            res.json({message:"No such request to be accepted"})
+        }
+
     }catch (err){
         res.json(err)
     }
@@ -121,9 +150,15 @@ removeFriend: async (req,res)=>{
     try{
         const uid = req.body.uid
         const aid = req.body.removeId
-        userCollection.updateOne({_id:mongoose.Types.ObjectId(uid)},{$pull:{friends:'auth0|'+ aid}})
-        userCollection.updateOne({_id:mongoose.Types.ObjectId(aid)},{$pull:{friends:'auth0|'+uid}})
-        res.json({message:"Friend removed"})
+        const check = await userCollection.findOne({_id:mongoose.Types.ObjectId(uid),friends:prefix + aid})
+        const check2 = await userCollection.findOne({_id:mongoose.Types.ObjectId(aid),friends:prefix + uid})
+        if (check != null && check2 != null){
+            userCollection.updateOne({_id:mongoose.Types.ObjectId(uid)},{$pull:{friends:prefix+ aid}})
+            userCollection.updateOne({_id:mongoose.Types.ObjectId(aid)},{$pull:{friends:prefix+uid}})
+            res.json({message:"Friend removed"})
+        }else{
+            res.json({message:"No such friend to be removed"})
+        }
     }catch (err){
         res.json(err)
     }
@@ -137,13 +172,13 @@ deleteUser: async (req,res)=>{
         var doc = await userCollection.findOne({_id:mongoose.Types.ObjectId(req.params.id)})
         if (doc != null){
             await userCollection.deleteOne({_id:mongoose.Types.ObjectId(req.params.id)})
-            await postCollection.deleteMany({"author.sub":"auth0|" + req.params.id})
-            userCollection.updateMany({friends:"auth0|" + req.params.id},{$pull:{friends:'auth0|'+req.params.id}})
+            await postCollection.deleteMany({"author.sub":prefix + req.params.id})
+            userCollection.updateMany({friends:prefix + req.params.id},{$pull:{friends:prefix+req.params.id}})
             userCollection.updateMany({friendRequest:req.params.id},{$pull:{friendRequest:req.params.id}})
             getToken().then((data)=>{
                 TOKEN = data.access_token
                 var options = { method: 'DELETE',
-                url: 'https://dev-1ksx3uq3.us.auth0.com/api/v2/users/'+ "auth0|" + req.params.id,
+                url: 'https://dev-1ksx3uq3.us.auth0.com/api/v2/users/'+ prefix + req.params.id,
                 headers: { 'authorization':'Bearer ' + TOKEN, 'content-type': 'application/json' },
                 body: {},
                 json:true};
